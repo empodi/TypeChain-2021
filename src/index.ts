b68a723df58c719b16f52d7328d93fb01e9a7a13
empodi <kglein9513@gmail.com>
2021-10-08 00:16:45 +0900

    Create BlockChain 2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index:number;
     public hash:string;
     public previousHash:string;
     public data:string;
     public timestamp:number;
+
+    static calculateBlockHash = (
+        index:number, 
+        previousHash:string, 
+        data:string, 
+        timestamp:number
+    ): string => CryptoJS.SHA256(index + previousHash + data + timestamp).toString();
+
 
     constructor(
         index:number,
@@ -22,8 +32,17 @@ class Block {
 
 const genesisBlock = new Block(0, "2020202020202", "", "Hello", 123456);
 
-let blockChain: [Block] = [genesisBlock];
+let blockChain: Block[] = [genesisBlock];
+
+const getBlockchain = () : Block[] => blockChain;
+
+const getLastestBlock = () : Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+
 
 console.log(blockChain);
+
+//npm add crypto-js   .... to create hash
 
 export{};
