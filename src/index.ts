af8dd355e7b7b3649803eb026923d7655f2c02cd
empodi <kglein9513@gmail.com>
2021-10-07 23:50:58 +0900

    how to use "class"

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log("hello");
+/* 
+###Interface### 
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+} */
+
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+
+    constructor(name:string, age:number, gender?:string) {  //  ? means it's optional
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
+const empodi = new Human("empodi", 26, "male");
+
+const sayHi = (person: Human) => {
+
+    return `Hello ${person.name}, you are ${person.age} and you are a ${person.gender}...`;
+};
+
+console.log(sayHi(empodi));
+
+export{};
